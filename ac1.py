f22bce2290aa4bdc7682ba4dd9ec5610ce631aeb
humayramehzabin009 <humayramehjabinshejuti@gmail.com>
2024-12-02 14:41:47 +0600

    Update ac1.py

--- ac1.py
+++ ac1.py
@@ -9,23 +9,3 @@ window.title('Demo Window')
 window.geometry('400x300')
 window.mainloop()
 
-# # Import necessary libraries
-# from tkinter import *
-
-
-# # Setup Tkinter Window
-# root = Tk()
-# root.geometry("200x200")
-
-# # Function for Displaying Warning Message
-# # This will be called once the button is clicked
-# # messagebox.showwarning("Window Name", "Text to be displayed")
-# def msg():
-# 	messagebox.showwarning("Alert", "Stop! Virus Found.")
-
-# # Adding Button Widget to Window
-# button = Button(root, text="Scan for Virus", command=msg)
-# button.place(x=40, y=80)
-
-# # Entering main event loop
-# root.mainloop()
